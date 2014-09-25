6d6e2cc8257f725e33fef384d578c6483ebb72de
Jim Hayes <jhayes@sdsc.edu>
2014-09-24 23:24:00 -0700

    Bug fix: mistakenly left constant VERSION defn in version.mk.

--- src/mkl-modules/version.mk
+++ src/mkl-modules/version.mk
@@ -1,12 +1,11 @@
 NAME        = mkl-modules
-VERSION     = 11.1.2.144
 RELEASE     = 2
 PKGROOT     = /opt/modulefiles/applications/mkl
-
-INTELVERS   = 2013_sp1.2.144
 
 VERSION_SRC = $(REDHAT.ROOT)/src/mkl/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
+
+INTELVERS   = 2013_sp1.2.144
 
 RPM.EXTRAS  = AutoReq:No
