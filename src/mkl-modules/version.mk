2925a308724b3cf8b7306f837db94cc6e20ccdc3
Jim Hayes <jhayes@sdsc.edu>
2014-10-01 10:45:38 -0700

    Use canonical modulefile/roll-test build files.

--- src/mkl-modules/version.mk
+++ src/mkl-modules/version.mk
@@ -1,8 +1,11 @@
-NAME        = mkl-modules
-RELEASE     = 2
-PKGROOT     = /opt/modulefiles/applications/mkl
+PACKAGE     = mkl
+CATEGORY    = applications
 
-VERSION_SRC = $(REDHAT.ROOT)/src/mkl/version.mk
+NAME        = $(PACKAGE)-modules
+RELEASE     = 3
+PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
+
+VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -1,5 +1,6 @@
 NAME       = mkl-roll-test
 VERSION    = 1
 RELEASE    = 1
+PKGROOT    = /root/rolltests
 
 RPM.EXTRAS = AutoReq:No
